53300f035a4dd57dff9ad5c9b8cbb87cc927c2ce
Horlard1 <horlarmihdeh@gmail.com>
2025-05-23 18:31:00 +0100

    added more styling, unique date, delet feature and edit feature

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 interface Todos {
   id: string;
   value: string;
+  completed: boolean;
 }
 
 export const apiSlice = createApi({
@@ -15,16 +16,38 @@ export const apiSlice = createApi({
       providesTags: ["todos"],
     }),
 
-    addTodos: builder.mutation<Todos, any>({
+    addTodos: builder.mutation<Todos, Partial<Todos>>({
       query: ({ id, value }) => ({
         url: `/todos`,
         method: "POST",
         body: { id, value },
       }),
+      invalidatesTags: ["todos"],
+    }),
+
+    deleteTodo: builder.mutation<{ id: string }, string>({
+      query: (id) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["todos"],
+    }),
+    updateTodo: builder.mutation<Todos, Partial<Todos>>({
+      query: ({ id, ...body }) => ({
+        url: `/todos/${id}`,
+        method: "PUT",
+        body: body,
+      }),
+      invalidatesTags: ["todos"],
     }),
   }),
 
   // define endpoints later
 });
 
-export const { useGetTodosQuery, useAddTodosMutation } = apiSlice;
+export const {
+  useGetTodosQuery,
+  useAddTodosMutation,
+  useDeleteTodoMutation,
+  useUpdateTodoMutation,
+} = apiSlice;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
-import { useAddTodosMutation, useGetTodosQuery } from "@/api/apiSlice";
-import Image from "next/image";
+import {
+  useAddTodosMutation,
+  useDeleteTodoMutation,
+  useGetTodosQuery,
+  useUpdateTodoMutation,
+} from "@/api/apiSlice";
 import React, { useState } from "react";
 
 export default function Home() {
   const { data, error, isLoading, refetch } = useGetTodosQuery();
   const [newListItem, setNewListItem] = useState("");
-  const [addPost, { status, isLoading: loadingPost }] = useAddTodosMutation();
+  const [addPost, { isLoading: loadingPost }] = useAddTodosMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
 
   return (
     <div className="w-full h-screen text-black justify-center items-center bg-gray-100 flex flex-col space-y-5">
@@ -23,8 +29,9 @@ export default function Home() {
           onClick={async () => {
             const trimmedValue = newListItem?.trim?.();
             await addPost({
-              id: data && data?.length + 1,
+              id: `${Date.now()}-${Math.floor(Math.random() * 1000)}`,
               value: trimmedValue,
+              completed: false,
             });
             setNewListItem("");
           }}
@@ -35,28 +42,90 @@ export default function Home() {
       </div>
       {isLoading ? (
         <div className="grid grid-cols-2 gap-5 ">
-          <div className="h-10 w-full bg-gray-200 animate-pulse"></div>
-          <div className="h-10 w-full bg-gray-200 animate-pulse"></div>
+          <div className="h-10 w-full bg-gray-400 animate-pulse"></div>
+          <div className="h-10 w-full bg-gray-400 animate-pulse"></div>
+        </div>
+      ) : error ? (
+        <div className="space-y-5 flex flex-col justify-center items-center border border-gray-300 p-5 rounded-lg">
+          <div className="w-10 h-10 font-bold rounded-full flex justify-center items-center border border-yellow-600 text-yellow-600">
+            !
+          </div>
+          <p className="text-xs">Failed to fetch data! Try again</p>
+          <button
+            onClick={() => {
+              refetch?.();
+            }}
+            className="px-4 rounded-lg py-2 bg-blue-600 text-white hover:opacity-70 text-xs"
+          >
+            Retry
+          </button>
         </div>
       ) : //@ts-ignore
       data?.length > 0 ? (
-        <div className="border border-gray-300 rounded-lg">
-          <table>
-            <thead>
+        <div className="relative w-full sm:w-1/2 mx-auto overflow-x-auto rounded border border-gray-400 shadow-md sm:border-gray-300">
+          <table className="relative w-full text-left text-sm text-gray-500 rtl:text-right ">
+            <thead className="bg-gray-300 text-xs font-medium text-gray-700 ">
               <tr>
-                <th>S/N</th>
-                <th>Todo</th>
-                <th></th>
+                <th
+                  scope="col"
+                  className="whitespace-nowrap px-5 py-2.5 text-xs"
+                >
+                  S/N
+                </th>
+                <th
+                  scope="col"
+                  className="whitespace-nowrap px-5 py-2.5 text-xs"
+                >
+                  Todo
+                </th>
+
+                <th
+                  scope="col"
+                  className="whitespace-nowrap px-5 py-2.5 text-xs"
+                ></th>
               </tr>
             </thead>
             <tbody>
-              {data?.map?.((item) => (
-                <tr key={item?.id}>
-                  <td>{item?.id}</td>
-                  <td>{item?.value}</td>
-                  <td>
-                    <button className="text-xs px-2 py-1.5 border rounded-lg hover:bg-red-600 hover:text-white">
-                      Remove
+              {data?.map?.((item, index) => (
+                <tr key={item?.id} className={` border-b hover:bg-gray-50`}>
+                  <th
+                    scope="row"
+                    className="whitespace-nowrap px-5 py-2.5 text-sm font-normal"
+                  >
+                    {index + 1}
+                  </th>
+                  <td
+                    className={`whitespace-nowrap px-5 ${
+                      item?.completed ? "line-through" : ""
+                    } py-2.5 text-sm`}
+                  >
+                    {item?.value}
+                  </td>
+
+                  <td className="whitespace-nowrap flex gap-2 items-center justify-end px-5 py-2.5">
+                    <button
+                      onClick={async () => {
+                        await updateTodo({
+                          id: item.id,
+                          completed: !item.completed,
+                          value: item?.value,
+                        });
+                      }}
+                      className={`flex items-center p-1 gap-1 rounded border text-xs ${
+                        item?.completed
+                          ? "border-gray-600 text-gray-600 hover:bg-gray-600"
+                          : "border-green-600 text-green-600 hover:bg-green-600"
+                      } hover:text-white hover:opacity-70`}
+                    >
+                      {item?.completed ? "Revert" : "Complete"}
+                    </button>
+                    <button
+                      onClick={async () => {
+                        await deleteTodo(item?.id);
+                      }}
+                      className="flex items-center gap-1 rounded border text-xs border-red-600 p-1 text-red-600 hover:bg-red-600 hover:text-white hover:opacity-70"
+                    >
+                      Delete
                     </button>
                   </td>
                 </tr>
